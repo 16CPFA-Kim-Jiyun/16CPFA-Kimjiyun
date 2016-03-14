8a75cc06a8921c7ef8c351b2fa833e2b6179edc0
Jiyun Kim <pang95pang@naver.com>
2016-03-14 23:42:13 +0900

    added ex01.py

---
+++ ex01/ex01.py
@@ -0,0 +1,10 @@
+print "Hello World"
+print "Computer Programming Fundamentals"
+print "I like typing this"
+print "This is fun"
+print 'Yay! Printing'
+print "I'd much rather you 'not'"
+print 'i "said" do not touch this.'
+
+print "Jiyun Kim"
+print "2016 03 09"
