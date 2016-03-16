df6f6d17f4330ccc522013517b52459101c28a9a
CAD Client <CAD Client>
2016-03-16 19:47:23 +0900

    added ex04.py drills error

--- ex04/ex04.py
+++ ex04/ex04.py
@@ -5,7 +5,7 @@ passengers = 90
 cars_not_driven = cars - drivers
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
-average_passengers_per_car = passengers / cars_driven
+average_passengers_per_car = cars_pool_capacity / passenger
 
 
 print "There are", cars, "cars available."
