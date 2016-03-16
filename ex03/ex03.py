bc7ceebe22bcce5fccff0ec0541d851ff3a29118
CAD Client <CAD Client>
2016-03-16 20:17:14 +0900

    added ex03.py drill 01 다시

--- ex03/ex03.py
+++ ex03/ex03.py
@@ -1,24 +1,28 @@
-print "I will now count my chickens:" # count chicken
+#-*-coding:cp949
 
+print "I will now count my chickens:"
+# 닭의 수를 세어봅시다
 print "Hens", 25 + 30 / 6 
 print "Roosters", 100 - 25 * 3 % 4 
-
+# 암탉, 수탉의 수
 print "Now I will count the eggs:" 
-
+# 달걀의 수를 세어봅시다
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
-
+# 달걀 갯수
 print "Is it true that 3 + 2 < 5 - 7?"
-
+# 3+2는 5-7보다 작은게 맞나요?
 print 3 + 2 < 5 - 7 
-
+# 결과
 print "What is 3 + 2?", 3 + 2
 print "What is 5 - 7?", 5 - 7 
-
+# 3+2, 5-7은 무엇인가요? 결과
 print "Oh, that's why it's False." 
-
+# 틀립니다
 print "How about some more." 
-
+# 더 해봅시다
 print "Is it greater?", 5 > -2 
 print "Is it greater or equal?", 5 >= -2 
 print "Is it less or equal?", 5<= -2 
-
+# 5는 -2보다 큽니까?
+# 5는 -2보다 같거나 큽니까?
+# 5는 -2보다 같거나 작습니까?
