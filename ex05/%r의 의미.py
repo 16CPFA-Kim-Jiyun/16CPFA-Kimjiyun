2a97bcf64373454b408aafcf85d3d33dbe3a1d7e
CAD Client <CAD Client>
2016-03-23 19:53:05 +0900

    %r의 의미

---
+++ ex05/%r의 의미.py
@@ -0,0 +1,17 @@
+Python 2.7.11 |Anaconda 2.5.0 (64-bit)| (default, Jan 29 2016, 14:26:21) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> eval('"abc"')
+'abc'
+>>> eval('8')
+8
+>>> eval('8+8')
+16
+>>> repr(8)
+'8'
+>>> repr('abc')
+"'abc'"
+>>> eval('[1,2,3,4]')
+[1, 2, 3, 4]
+>>> repr([1,2,3,4])
+'[1, 2, 3, 4]'
+>>> 
