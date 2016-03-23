2b964edbd4b505fca9b5f738c2a738b36bd67886
CAD Client <CAD Client>
2016-03-23 18:35:08 +0900

    ex05.py에 drills 1,2 추가

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -1,17 +1,17 @@
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie
-my_height = 74 # inches
-my_weight = 180 #lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+her_name = 'Jina'
+her_age = 28 # not a lie
+her_height_cm = 155
+her_weight_kg = 50
+her_eyes = 'Black'
+her_teeth = 'White'
+her_hair = 'Black'
 
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+print "Let's talk about %s." % her_name
+print "She's %d centimeters tall." % her_height_cm
+print "She's %d kilograms heavy." % her_weight_kg
 print "Actuallythat's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
+print "She's got %s eyes and %s hair." % (her_eyes, her_hair)
+print "Her teeth are usually %s depending on the coffee." % her_teeth
 
 # this line is tricky, try to get it exactly right
-print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+print "If I add %d, %d, and %d I get %d." % (her_age, her_height_cm, her_weight_kg, her_age + her_height_cm + her_weight_kg)
