f9b6047f2ac4bb63d032ff7e7e4d3996aa02bc26
CAD Client <CAD Client>
2016-03-23 18:50:09 +0900

    %d, %s 의미

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -1,15 +1,15 @@
 her_name = 'Jina'
 her_age = 28 # not a lie
-her_height_cm = 155
+her_height_cm = 155.2
 her_weight_kg = 50
 her_eyes = 'Black'
 her_teeth = 'White'
 her_hair = 'Black'
-
+# place holder
 print "Let's talk about %s." % her_name
-print "She's %d centimeters tall." % her_height_cm
+print "She's %g centimeters tall." % her_height_cm
 print "She's %d kilograms heavy." % her_weight_kg
-print "Actuallythat's not too heavy."
+print "Actually that's not too heavy."
 print "She's got %s eyes and %s hair." % (her_eyes, her_hair)
 print "Her teeth are usually %s depending on the coffee." % her_teeth
 
