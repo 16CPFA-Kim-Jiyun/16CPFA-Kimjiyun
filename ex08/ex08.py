8d9dd676b2a72c2207c2ec18bb78ea1c041eb1ef
CAD Client <CAD Client>
2016-03-30 18:40:58 +0900

    ex08.py에 formatter % 에서 ' '가 아니라 " "으로 나오는 이유

--- ex08/ex08.py
+++ ex08/ex08.py
@@ -1,3 +1,4 @@
+#-*-coding:cp949
 formatter = "%r %r %r %r"
 
 print formatter % (1, 2, 3, 4)
@@ -7,5 +8,6 @@ print formatter % (formatter, formatter, formatter, formatter)
 print formatter % ("I had this thing.",
                    "That you could type up right.",
                    "But it didn't sing.",
+                   # 문장안에 작은 따음표가 있어서 큰따음표로 출력됨
                    "So I said goodnight.")
 
