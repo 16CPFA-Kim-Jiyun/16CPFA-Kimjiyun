50efc33c1d1cce78d8ee6a18e710f89013dcf7ec
CAD Client <CAD Client>
2016-03-30 18:16:21 +0900

    ex07.py에 study drils 추가

--- ex07/ex07.py
+++ ex07/ex07.py
@@ -1,7 +1,13 @@
+#-*-coding:cp949
+
 print "Mary had a little lamb."
+# 메리는 작은 양을 가졌다.
 print "Its fleece was white as %s." % 'snow'
+# 양털은 눈처럼 하얗다.
 print "And everywhere that Mary went."
-print "." * 10 # what'd that do?
+# 메리가 가는곳어디든지
+print ".*" * 10 # what'd that do?
+# ....무엇을할까 , .*이 열번나온다.
 
 end1 = "C"
 end2 = "h"
@@ -15,7 +21,9 @@ end9 = "r"
 end10 = "g"
 end11 = "e"
 end12 = "r"
+# 변수를 나열
 
 # watch that comma at the end. try removing it to see what happens
 print end1 + end2 + end3 + end4 + end5 + end6,
 print end7 + end8 + end9 + end10 + end11 + end12
+# ,는 이어져서 나오게 한다.
