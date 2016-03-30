fc7dea9d0434609bc567b399b0b83caf657d43d5
CAD Client <CAD Client>
2016-03-30 20:09:57 +0900

    ex11.py study drills 다시 추가

--- ex11/ex11.py
+++ ex11/ex11.py
@@ -10,3 +10,9 @@ print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
 
 print "Are you hungry?",
 answer = raw_input()
+print "How much money do you have in your poket?",
+answer = raw_input()
+print "What do you want?",
+food = raw_input()
+
+print "So, you have to go to store and buy some %s." % food
