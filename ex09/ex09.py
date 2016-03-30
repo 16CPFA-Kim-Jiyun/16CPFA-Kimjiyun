dfff6f22e5aa84af7950c6ba9190afb9b7cafa03
CAD Client <CAD Client>
2016-03-30 18:59:39 +0900

    알아야할내용추가

--- ex09/ex09.py
+++ ex09/ex09.py
@@ -1,7 +1,10 @@
+#-*-coding:cp949
+
 # Here's some new strange stuff, remember type it exactly.
 
 days = "Mon Tue Wed Thu Fri Set Sun."
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+# \는 backslash, \n은 새로운 라인을 줌.
 
 print "Here are the days: ", days
 print "Here are the months: ", months
@@ -12,3 +15,4 @@ With the three double-quotes.
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
 """
+# """ """ 사이에 '," 다 들어갈수있고 여러 문자열 가능.
