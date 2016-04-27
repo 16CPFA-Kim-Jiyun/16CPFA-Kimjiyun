dd48d6576c82fcaf5e8e776d40cda73f94f5675a
CAD Client <CAD Client>
2016-04-27 19:51:41 +0900

    ex21.py 주석 추가

--- ex21/ex21.py
+++ ex21/ex21.py
@@ -3,19 +3,19 @@
 def add(a, b):
     print("ADDING %d + %d" % (a, b))
     return a + b
-
+# return의 역할 : (반환역할=)계산결과를 다른 함수로 받아서 쓸 수 있게 해준다. 21번째줄부터~~받아진다.
 def subtract(a, b):
     print("SUBTRACTING %d - %d" % (a, b))
     return a - b
-
+# a - b 값이 22째줄
 def multiply(a, b):
     print("MULTIPLYING %d * %d" % (a, b))
-    return a / b
-
+    return a * b
+# a*b 값이 23째줄
 def divide(a, b):
     print("DIVIDING %d / %d" % (a, b))
     return a / b
-
+# a / b 값이 24쩨즐
 print("Let's do some math with just functions!")
 
 age = add(30, 5)
