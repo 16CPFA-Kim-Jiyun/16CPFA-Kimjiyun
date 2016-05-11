5648d72c4fd79fdf96e5711aec585d60c6bc4e59
CAD Client <CAD Client>
2016-05-11 19:55:45 +0900

    added ex33.py

---
+++ ex33/ex33.py
@@ -0,0 +1,19 @@
+# -*-coding:utf8
+
+i = 0
+numbers = []
+
+# while 문은 반복해서 문장을 수행할때 쓴당(무한루프)
+while i < 6:
+    print "At the top i is %d" % i
+    numbers.append(i)
+
+    i = i + 1
+    print "Numbers now: ", numbers
+    print "At the bottom i is %d" % i
+
+
+print "The numbers: "
+
+for num in numbers:
+    print num
