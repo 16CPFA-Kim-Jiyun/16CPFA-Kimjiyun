832708c6e90d8c5b49c6502c66dc7f68c2bcab80
CAD Client <CAD Client>
2016-05-11 18:51:08 +0900

    es31 study drill

--- ex31/ex31.py
+++ ex31/ex31.py
@@ -1,3 +1,5 @@
+# -*-coding:utf8
+
 print "You enter a dark room with two doors. Do you through door #1 or door #2?"
 
 door = raw_input("> ")
@@ -6,6 +8,7 @@ if door == "1":
     print "There's a giant bear here eating a cheese cake. What do you do?"
     print "1. Take the cake."
     print "2. Scream at the bear."
+    print "3. Hit the bear"
 
     bear = raw_input("> ")
 
