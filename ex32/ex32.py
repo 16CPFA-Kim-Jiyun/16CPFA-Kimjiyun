c42134d46ae750387dfceef2c545f1bd0bb69540
CAD Client <CAD Client>
2016-05-11 19:37:17 +0900

    ex32.py study drill

--- ex32/ex32.py
+++ ex32/ex32.py
@@ -3,6 +3,7 @@
 the_count = [1, 2, 3, 4, 5]
 fruits = ['apples', 'oranges', 'pears', 'apricots']
 change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+# 리스트 작성 []로!
 
 # this first kind of for-loop goes through a list
 for number in the_count:
@@ -20,11 +21,18 @@ for i in change:
 # we can also build lists, first start with an empty one
 elements = []
 
-# then use the range function to do 0 to 5 counts
+# then use the range function to do 0  to 5 counts
 for i in range(0, 6):
     print "Adding %d to the list." % i
     # append is a function that lists understand
     elements.append(i)
+
+# 또는 python 에서는 아래와 같이 할 수도 있다.
+# list comprehension
+elements2 = [i for i in range(0, 6)]
+
+# range 단독으로 써도 됩니다아~
+range(0, 6)
 
 # now we can print them out too
 for i in elements:
